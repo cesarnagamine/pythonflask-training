f7f3f21a643755409736ca44ff8b6ba72ddeaac7
unknown <cesarnagamine@gmail.com>
2020-05-27 00:48:19 -0500

    ''

---
+++ send_mail.py
@@ -0,0 +1,22 @@
+import smtplib
+from email.mime.text import MIMEText
+
+
+def send_mail(customer, dealer, rating, comments):
+    port = 2525
+    smtp_server = 'smtp.mailtrap.io'
+    login = 'b733c7db96750b'
+    password = 'b2e2bc60f37916'
+    message = f"<h3>New Feedback Submission</h3><ul><li>Customer: {customer}</li><li>Dealer: {dealer}</li><li>Rating: {rating}</li><li>Comments: {comments}</li></ul>"
+
+    sender_email = 'email1@example.com'
+    receiver_email = 'email2@example.com'
+    msg = MIMEText(message, 'html')
+    msg['Subject'] = 'Universal Service Feedback'
+    msg['From'] = sender_email
+    msg['To'] = receiver_email
+
+    # Send email
+    with smtplib.SMTP(smtp_server, port) as server:
+        server.login(login, password)
+        server.sendmail(sender_email, receiver_email, msg.as_string())
